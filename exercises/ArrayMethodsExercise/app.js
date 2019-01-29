6c6f9805974b6cc8a665db7b21a1a4391b6c0c77
Ani Turner <aniturner93@gmail.com>
2019-01-28 20:05:51 -0700

    commit message

---
+++ exercises/ArrayMethodsExercise/app.js
@@ -0,0 +1,22 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+vegetables.pop()
+// console.log(vegetables)
+fruit.shift()
+// console.log(fruit)
+fruit.indexOf("orange")
+// console.log(fruit)
+fruit.push("1")
+// console.log(fruit)
+var length = vegetables.length
+// console.log(length)
+vegetables.push(length)
+// console.log(vegetables)
+var food = fruit.concat(vegetables)
+// console.log(food)
+food.splice(4, 2)
+// console.log(food)
+food.reverse()
+// console.log(food)
+var reversed = food.join(",")
+// console.log(reversed)
---
+++ exercises/loop-olympics/app.js
@@ -0,0 +1,110 @@
+// Write an if statement that prints "is greater than" if 5 is greater than 3
+// if(5 > 3){
+//     console.log("is greater than")
+// }else{
+//     console.log("is not greater than")
+// }
+
+// Write an if statement that prints "is the length" if the length of "cat" is 3
+// var cat = 1;2;3
+// if (cat.length = 3){
+//     console.log("is the length") 
+// } else {
+//     console.log("is not")
+// }
+
+// Write an if/else statement that checks if "cat" is equal to "dog" and prints, "not the same" when they are not equal.
+
+// var animals = "cat"; "dog"
+
+// if ("cat" == "dog") {
+//     console.log("the same")
+// } else {
+//     console.log("not the same")
+// }
+
+
+// Using the below object, write an if statement that prints <theNameOfThePersonInObject> is allowed to go to the movie if they are old enough (18 or older), and the opposite if they are not older than 18.
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+// if (person.age > 18) {
+//     console.log(person.name + " is allowed to go to the movie if they are old enough")
+// } else {
+//     console.log (person.name + " is not old enough to go the movie.")
+// }
+// Using that same object, only allow them into the movie if their name starts with "B"
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// } 
+// var n = person.name.startsWith("B");
+
+// if (n) {
+//     console.log("they are allowed into the movie")
+// } else {
+//     console.log("they are not allowed into the movie")
+// }
+
+// Using that same object, only allow them into the movie if their name starts with "B" and they are older than 18.
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+// var n = person.name.startsWith("B")
+
+// if (n && person.age > 18) {
+//     console.log("they are allowed into the movie")
+// } else {
+//     console.log("is not allowed into movie")
+// }
+
+// Write an if/else if/else statement that prints "strict" if 1 strictly equals "1", prints "loose" or "abstract" if 1 equals "1" without type checking, and prints "not equal at all" if it doesn't print the other stuff.
+// if (1 === "1") {
+//     console.log("strict") 
+// } else if (1 == "1") {
+//     console.log("loose")
+// } else {
+//     console.log("not equal at all")
+// }
+
+
+// Write an if statement that prints "yes" if 1 is less than or equal to 2 and 2 is equal to 4 OR!!! 3 * 4 is greater than 10 and 5 plus 10 is greater than 10.
+// if (1 <= 2 && 2 == 4 || 3 * 4 > 10 && 5 + 10 > 10) {
+//     console.log("yes")
+// }
+
+// Write an if statement that checks to see if "dog" is a string
+// if ("dog" === typeof string)
+//     console.log("true")
+
+// Write an if statement that checks to see if "true" is a boolean
+// if ("true" == typeof boolean) {
+//     console.log("true") 
+// } else {
+//     console.log("not true")
+// }
+
+// Write an if statement that checks to see if a variable has been defined or not
+// if (typeof variable === 'undefined') {
+//     console.log("variable is undefined")
+// } else {
+//     console.log("variable is defined")
+// }
+
+// Write an if statement comparing letters to numbers using < and >. Try to figure out what letters are going to be "greater than" other numbers or letters. For example: is "s" greater than 12?
+// if ("time" > 2) {
+//     console.log("letter is greater than number")
+// } else {
+//     console.log("number is greater than letter")
+// }
+
+// Write a ternary statement that console.logs whether a number is odd or even. For example:
+// var myNum = 10;
+// if (myNum % 2){
+//     console.log("my number is odd")
+// } else {
+//     console.log("my number is even")
+// }
