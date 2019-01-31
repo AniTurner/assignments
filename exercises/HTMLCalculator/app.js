bdbfb8987e6a73b55fdda4e92f99cff0b4a52c64
Ani Turner <aniturner93@gmail.com>
2019-01-30 20:57:42 -0700

    commit message

---
+++ exercises/HTMLCalculator/app.js
@@ -0,0 +1,8 @@
+var add=document.getElementById("add")
+add.addEventListener("submit", function(event){
+
+})
+var add = document.getElementById("add")
+add.addEventListener("mouseover", function(event){
+    event.style = "background-color: light-blue"
+})
---
+++ exercises/dom-practice/app.js
@@ -0,0 +1,4 @@
+var myH1 = document.createElement('h1')
+myH1.textContent ="greeting"
+var container = document.getElementById('container')
+container.appendChild(myH1)
