6765d38188a5e5749831a621dc02ee3d1579918f
Ani Turner <aniturner93@gmail.com>
2019-02-04 09:39:21 -0700

    commit message

--- exercises/PracticeEdabit/app.js
+++ exercises/PracticeEdabit/app.js
@@ -1,17 +1,34 @@
 // Create a function that takes two numbers as arguments and return their sum.
 
-function addition(a, b) {
-    return a + b
-}    
-console.log(addition(2,3))
-console.log(addition(-3,-6))
-console.log(addition(7,3))
+// function addition(a, b) {
+//     return a + b
+// }    
+// console.log(addition(2,3))
+// console.log(addition(-3,-6))
+// console.log(addition(7,3))
 
 // Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 
-function increment(a) {
-    return a + 1
+// function increment(a) {
+//     return a + 1
+// }
+// console.log(increment(0))
+// console.log(increment(9))
+// console.log(increment(-3))
+
+//Write both a function expression and a function declaration
+    //You cannot use console.log() in the function when it's done
+    //Function must return a value
+    //Console.log the called function to see the returned value
+
+function myNum(a,b) {
+    return a * b
 }
-console.log(increment(0))
-console.log(increment(9))
-console.log(increment(-3))
+console.log(myNum(2,4))
+
+var myNum = function() {
+    var a = 3
+    var b = 2
+    return a * b
+}
+console.log(myNum())
