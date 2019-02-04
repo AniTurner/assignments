94c4d581f0871a16938cfe8a317e36a946efec8a
Ani Turner <aniturner93@gmail.com>
2019-02-04 09:02:22 -0700

    commit message

---
+++ exercises/PracticeEdabit/app.js
@@ -0,0 +1,17 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function addition(a, b) {
+    return a + b
+}    
+console.log(addition(2,3))
+console.log(addition(-3,-6))
+console.log(addition(7,3))
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function increment(a) {
+    return a + 1
+}
+console.log(increment(0))
+console.log(increment(9))
+console.log(increment(-3))
