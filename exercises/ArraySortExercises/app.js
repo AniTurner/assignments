5de977f54a56a58e58bbf85c8061027f2e331e19
Ani Turner <aniturner93@gmail.com>
2019-02-08 13:49:25 -0700

    commit message

---
+++ exercises/ArraySortExercises/app.js
@@ -0,0 +1,10 @@
+//1) Sort an array from smallest number to largest
+
+function leastToGreatest(arr) {
+    var sorted = arr.sort(function(a,b){
+        return a-b 
+    })
+  } 
+  
+  console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90] 
+  
