a294b82f811d06259d2900bda01d486c366c2bab
Ani Turner <aniturner93@gmail.com>
2019-02-08 18:08:49 -0700

    commit message

--- exercises/ArrayReduceExercises/app.js
+++ exercises/ArrayReduceExercises/app.js
@@ -68,50 +68,70 @@
 
 //5) Given an array of arrays, flatten them into a single array
 
-function flatten(arr) {
-    var all = arr.reduce(function(a,b) {
-        return a.concat(b)
-    })   
-    return all
- } 
+// function flatten(arr) {
+//     var all = arr.reduce(function(a,b) {
+//         return a.concat(b)
+//     })   
+//     return all
+//  } 
  
- var arrays = [
-     ["1", "2", "3"],
-     [true],
-     [4, 5, 6]
- ];
+//  var arrays = [
+//      ["1", "2", "3"],
+//      [true],
+//      [4, 5, 6]
+//  ];
  
- console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+//  console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
  
 
 
  //6) Given an array of potential voters, return an object representing the results of the vote
-//  var voters = [
-//     {name:'Bob' , age: 30, voted: true},
-//     {name:'Jake' , age: 32, voted: true},
-//     {name:'Kate' , age: 25, voted: false},
-//     {name:'Sam' , age: 20, voted: false},
-//     {name:'Phil' , age: 21, voted: true},
-//     {name:'Ed' , age:55, voted:true},
-//     {name:'Tami' , age: 54, voted:true},
-//     {name: 'Mary', age: 31, voted: false},
-//     {name: 'Becky', age: 43, voted: false},
-//     {name: 'Joey', age: 41, voted: true},
-//     {name: 'Jeff', age: 30, voted: true},
-//     {name: 'Zack', age: 19, voted: false}
-// ];
+ var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
 
-// function voterResults(arr) {
-//     var allVotes = arr.reduce(function(youthVotes, person){
-//         if(youthVotes.age > 18 && youthVotes.age < 21) {
-//             youthVotes + person
-//         }
-//         return youthVotes
-//     }, {youngVotes: 0, midVotes:0, mids: 0, oldVotes: 0, olds: 0}) 
-//     return allVotes
-// }
+function voterResults(arr) {
+    var allVotes = arr.reduce(function(final, person){
+        if(person.age >= 18 && person.age <= 25) {
+            final.youth++;
+            if(person.voted){
+                final.youngVotes++
+            }
+        }
+        if(person.age >= 26 && person.age <= 35) {
+            final.mids++;
+            if(person.voted) {
+                final.midVotes++
+            }
+        }
+        if(person.age >= 36 && person.age <= 55) {
+            final.olds++;
+            if(person.voted) {
+                final.oldVotes++
+            }
+        }
+        return final
+    }, {youngVotes: 0, 
+        youth: 0, 
+        midVotes:0, 
+        mids: 0, 
+        oldVotes: 0, 
+        olds: 0}) 
+    return allVotes
+}
 
-// console.log(voterResults(voters)); // Returned value shown below:
+console.log(voterResults(voters)); // Returned value shown below:
 /*
 { youngVotes: 1,
   youth: 4,
