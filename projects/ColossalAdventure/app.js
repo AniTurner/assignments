f0fce9405e6ba69ae6f4a21440408355bd6d8d47
Ani Turner <aniturner93@gmail.com>
2019-02-10 14:51:14 -0700

    commit message

--- projects/ColossalAdventure/app.js
+++ projects/ColossalAdventure/app.js
@@ -1,24 +1,61 @@
-var ask = require('readline-sync')
+var ask = require('readline-sync'),
 
+///////////////////Global variables
+////////Choose a player////////////////////////////////////////
+intro = ask.question("Welcome to the game of ice and fire. WHEN YOU PLAY THE GAME OF THRONES, YOU WIN OR YOU DIE. Click enter to give us your favorite Game of Thrones character's name.");
+
+// players = ["Jon Snow", "Daenerys Targaryen", "Cersei Lannister", "Aria Stark", "Jorah Mormont"],
+
+
+// character = ask.keyInSelect(players, 'Which character would you like to be?' )
+
+
+/////////////////Jon Snow/////////////////////////
+// if (character === 0) {
+//     console.log("Welcome to House Stark. WINTER IS COMING! ")
+// }
+
+/////////////////Daenerys//////////////////////////
+// if (character === 1) {
+//     console.log("Welcome to House Targaryen, Daenerys Stormborn of the House Targaryen, First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons. ")
+// }
+//////////////////Cersei///////////////////
+// if (character === 2) {
+//     console.log("Welcome to House Lannister, HEAR ME ROAR")
+// }
+//////////////////Aria///////////////////////////
+// if (character === 3) {
+//     console.log("Welcome to House Stark, youngest daughter of Lady Catelyn and Lord Eddard Stark")
+// }
+////////////////////Jorah////////////////////////
+// if (character === 4) {
+//     console.log("Welcome to House Mormont, courageous knight!")
+// }
+//////////////////////////
 var player = {
-    name: " ",
-    health: 100
+    name: "",
+    health: 100,
+    inventory: []
 } 
+
+var attackers = ["White Walkers", "House Baratheon", "Walder Frey"]
+///////////////////Game Functions////////////////////////////
 function walk() {
+    ///////Random number between 1-4
     var chance = Math.ceil(Math.random() * 4)
+    ///////If number is 1 --> attack!!
         if (chance === 1) {
-            console.log("Attack")
+            console.log("Let us ATTAAAAACKKKK!!!")
 
         } else {
-            console.log("Nothing happened")
+            console.log("Oh, nothing happened. You shall see!")
         }
 }
-
-player.name = ask.question("What is your name? ");
-console.log("Welcome " + player.name + " to my super awesome game")
+////////////////////Game Intro//////////////////////////////
+console.log("Let's get started " + player.character + " to my super awesome game")
 
 var options = ["Walk"]
-
+////////////////////Game Loop///////////////////////////////
 while(player.health > 0) {
     var userChoice = ask.keyInSelect(options, "What would you like to do? ")
     if(userChoice === 0) {
@@ -26,5 +63,6 @@ while(player.health > 0) {
         if (chance === 1) {
             walk()
         } else if (userChoise === 1)
+            console.log("You are getting attacked by " + attackers.Math.random())
     }
 }
