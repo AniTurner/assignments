f31ba397e4203dfbcb4701e2d3386bd843128901
Ani Turner <aniturner93@gmail.com>
2019-02-10 19:00:23 -0700

    commit message

--- exercises/PracticeEdabit/app.js
+++ exercises/PracticeEdabit/app.js
@@ -46,14 +46,22 @@
 
 // Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
-function lessThanOrEqualToZero(num) {
-    if (num <= 0) {
-        return true
-    }
-    return false
-}
-console.log(lessThanOrEqualToZero(5))
-console.log(lessThanOrEqualToZero(0))
-console.log(lessThanOrEqualToZero(-2))
-console.log(lessThanOrEqualToZero(1))
+// function lessThanOrEqualToZero(num) {
+//     if (num <= 0) {
+//         return true
+//     }
+//     return false
+// }
+// console.log(lessThanOrEqualToZero(5))
+// console.log(lessThanOrEqualToZero(0))
+// console.log(lessThanOrEqualToZero(-2))
+// console.log(lessThanOrEqualToZero(1))
 
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
+function calculateExponent(num1, num2) {
+    return Math.pow(num1,num2)
+}
+console.log(calculateExponent(5, 5))
+console.log(calculateExponent(10, 10))
+console.log(calculateExponent(3, 3))
