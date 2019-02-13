93c13b82e216671a16839c851b26c9e8fe66a2f4
Ani Turner <aniturner93@gmail.com>
2019-02-13 09:03:24 -0700

    commit message

--- exercises/PracticeEdabit/app.js
+++ exercises/PracticeEdabit/app.js
@@ -169,14 +169,13 @@
 //Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
 
 function reverse(bool) {
-    if(bool = typeof true ) {
-        console.log(false)
-    } else if (bool = typeof false) {
-        console.log(true)
-    } else if (bool != typeof true && bool != typeof false) {
-        console.log("boolean expected")
+    if(bool === true ) {
+        return (false)
+    } else if (bool === false) {
+        return (true)
+    } else if (bool != true && bool != false) {
+        return ("boolean expected")
     }
-    return bool
 }
 
 console.log(reverse(true)),
