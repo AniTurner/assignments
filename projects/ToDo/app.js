e6dfe34b4c39b6fcf7341fd8425d3fce459a382f
Ani Turner <aniturner93@gmail.com>
2019-02-14 16:52:54 -0700

    commit message

--- projects/ToDo/app.js
+++ projects/ToDo/app.js
@@ -1,31 +1,63 @@
-import Axios from "axios";
+// import axios from "axios";
 
-const toDoContainer = document.getElementById('toDo-list-container')
+const todoListContainer = document.getElementById('toDo-list-container')
+const todoForm = document["add-todo-form"]
 
 //package axios to get our data from API
 // const data = axios.get('https://api.vschool.io/ani/todo')  //our data stores pending promise this way
 
 //that is why we need to write
-Axios.get('https://api.vschool.io/ani/todo').then(response => {
+//to reuse this make it a function
+function getData() {
+    axios.get('https://api.vschool.io/ani/todo').then(response => {
     // console.log(reponse) //will show all the object in console, we need data
     // console.log(reponse.data) // put into a variable
-    const todos = response.data
-    listTodos(todos) //will show it is not defined, there we need to define it in another function
+        const todos = response.data
+        listTodos(todos) //will show it is not defined, there we need to define it in another function
 
-})
+    }).catch(err => console.log(err))
+}
 
 function listTodos(todosArr) {
     // console.log(todosArr) //will show empty objects
     //make 1 todo show up, then reuse that code to make all todos show up
-    const firstTodo = todosArr[0]
+    // const firstTodo = todosArr[0] //take this out and add index in todosArr to make it show each one
+    for(let i = 0; i < todosArr.length; i++) {
 
-    //How to make it show on the DOM
-    //Create Elements
-    const title = document.createElement('h1')
+        //How to make it show on the DOM
+        //Create Elements
+        const todoContainer = document.createElement('div')
+        const title = document.createElement('h1')
+        const imgUrl = document.createElement('img')
 
-    //Edit the element/ give it content
-    title.textContent = firstTodo.title
+        //Edit the element/ give it content
+        todoContainer.classList.add('todo-container')
+        title.textContent = todosArr[i].title
+        imgUrl.setAttribute('src', todosArr[i].imgUrl)
+        if(todosArr[i].completed) {
+            title.style.textDecoration = "line-through"
+        }
     
-    //Append it to the DOM
-    toDoContainer.appendChild(title)
+        //Append it to the DOM
+        todoContainer.appendChild(title)
+        todoContainer.appendChild(imgUrl)
+        todoListContainer.appendChild(todoContainer)
+    }
 }
+
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTodo = {
+        //always an object
+        title:  todoForm.title.value
+    }
+    todoForm.title.value = ""
+
+    //axios request POST
+    axios.post('https://api.vschool.io/ani/todo', newTodo).then(response => {
+        todoListContainer.innerHTML = ""
+        getData()
+    })
+})
+
