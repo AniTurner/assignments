84203c63761a35b5493f83df37b354bbb95833d3
Ani Turner <aniturner93@gmail.com>
2019-02-15 12:09:43 -0700

    commit message

--- exercises/SlopeStyle-ES6-Practice/app.js
+++ exercises/SlopeStyle-ES6-Practice/app.js
@@ -44,7 +44,7 @@
 // }
 // console.log(returnFirst([1,2,3,4,5]))
 
-
+//----------------------------------------------------------------------------//
 //Write destructuring code to assign variables that will help us return the expected string. Also, change the string to be using Template literals:
 
 // const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
@@ -54,3 +54,44 @@
 //     return `My top three favorite activities are,${firstFav },${secondFav} , and ${thirdFav}`
 // }
 // console.log(returnFavorites(favoriteActivities))
+
+//----------------------------------------------------------------------------//
+//Use the Rest and Spread Operator to help take any number of arrays, and return one array. You will need to change how the arrays are passed in. You may write it assuming you will always recieve three arrays if you would like to
+
+// function combineAnimals(arr1, arr2, arr3) {  
+//     return [...arr1,...arr2,...arr3]
+// }
+
+// const realAnimals = ["dog", "cat", "mouse"];
+// const magicalAnimals = ["jackolope"];
+// const mysteriousAnimals = ["platypus"];
+
+// console.log(combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals))
+
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+//----------------------------------------------------------------------------//
+
+//Try to make the following function more ES6xy:
+
+// const product = (a, b, c, d, e) => {
+//     let numbers = [a,b,c,d,e]
+//     return numbers.reduce((acc, number) => {
+//       return acc * number;
+//     }, 1)}
+  
+
+// console.log(product(1,2,3,4,5))
+
+//----------------------------------------------------------------------------//
+//Make the following function more ES6xy. Use at least both the rest and spread operators:
+
+// const unshift = (array, a, b, c, d, e) =>  
+//      {return [...array, a, b, c, d, e]}
+  
+// console.log(unshift(["blue", "red", "yellow", "pink"],"a", "b","c", "d", "e"))
+
+
+//----------------------------------------------------------------------------//
+//Write some destructuring code to help this function out. Use object literals to simplify it:
+
