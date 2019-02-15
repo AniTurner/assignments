2fb2fc6a69cba2b7bbd7254a12009385f4c7cde5
Ani Turner <aniturner93@gmail.com>
2019-02-15 10:54:45 -0700

    commit message

--- exercises/SlopeStyle-ES6-Practice/app.js
+++ exercises/SlopeStyle-ES6-Practice/app.js
@@ -1,3 +1,5 @@
+//----------SLOPES STYLE ES6 PRACTICE------------------------------------//
+
 // Use the Rest Operator to help this function return an array of animals, no matter how many animals are passed to it:
 
 // function collectAnimals(...animals) {  
@@ -7,6 +9,7 @@
 // collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"); 
 // ["dog", "cat", "mouse", "jackolope", "platypus"]
 
+//----------------------------------------------------------------------------//
 //Write a function that returns a food object with the array names as properties using Object Literals:
 
 // function combineFruit(fruit, sweets, vegetables){
@@ -18,16 +21,26 @@
 //              ["cake", "pie"],
 //              ["carrit"]))
 
+//----------------------------------------------------------------------------//
 //Use destructuring to fill in the blanks and use the property names as variables:
 
-const vacation = {  
-    location: "Burly Idaho",
-    duration: "2 weeks"
-  };
+// const vacation = {  
+//     location: "Burly Idaho",
+//     duration: "2 weeks"
+//   };
   
-  function parseSentence(){
-    const {location, duration} = vacation
-    return `We're going to have a good time in ${location} for ${duration}`
-  }
-console.log(parseSentence())
+//   function parseSentence(){
+//     const {location, duration} = vacation
+//     return `We're going to have a good time in ${location} for ${duration}`
+//   }
+// console.log(parseSentence())
+//----------------------------------------------------------------------------//
+//Use destructuring to make this code ES6:
+
+
+function returnFirst(items){
+    const [firstItem] = items /*change this line to be es6*/
+    return firstItem
+}
+console.log(returnFirst([1,2,3,4,5]))
 
