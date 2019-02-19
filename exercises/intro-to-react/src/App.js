d9d57829e3d50fd480dc2c9398b48fe6d166d08d
Ani Turner <aniturner93@gmail.com>
2019-02-19 11:28:26 -0700

    commit message

---
+++ exercises/intro-to-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './Nav.js'
+import './style.css'
+
+
+
+
+function App() {
+    return (
+        <div>
+            <Nav />
+            <h1 className='hello-world'>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ exercises/intro-to-react/src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='nav-bar'>
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+        </div>
+    )
+}
+
+export default Nav;
