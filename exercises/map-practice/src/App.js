bbf9bec4c421785ce78e882f8fd1b654e8688c47
Ani Turner <aniturner93@gmail.com>
2019-02-21 10:32:43 -0700

    commit message

--- exercises/map-practice/src/App.js
+++ exercises/map-practice/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Names from './Names.js'
 
 const App = () => {
-    const names = ['Susie', 'John', 'Elias']
+    const names = ['Susie','Susie', 'John', 'Elias']
+    const color = [{backgroundColor: 'blue'}, {backgroundColor: 'pink'}, {backgroundColor: 'green'}]
+     
     
+        
     return (
         <ul>
-            {names.map(name => <li>{name}</li>)}
+            {names.map((name, i) => <Names style={color[Math.floor(Math.random() *color.length)]} name = {name} key={i}/>)}
+           
+                
         </ul>
     )
 }
--- exercises/map-practice/src/Names.js
+++ exercises/map-practice/src/Names.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 
-const Names = () => {
+const Names = (props) => {
+    return (
+        <div style={props.style} >
+            <h3>{props.name}</h3>
+        </div>
+    )
    
 }
 
