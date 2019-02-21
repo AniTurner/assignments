7542e309fed1a9f447f8d9bbdb830e7557443419
Ani Turner <aniturner93@gmail.com>
2019-02-20 19:39:28 -0700

    commit message

--- exercises/PracticeEdabit/app.js
+++ exercises/PracticeEdabit/app.js
@@ -184,3 +184,18 @@
 // console.log(reverse(null))
 
 
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+//Create a function that takes an array of numbers and return the first and last elements as a new array.
+
+function firstLast(arr) {
+    return [arr.shift(), arr.pop()]
+}
+
+
+
+console.log(firstLast([5, 10, 15, 20, 25]))  //[5, 25]
+
+console.log(firstLast(["edabit", 13, null, false, true])) //["edabit", true]
+
+console.log(firstLast([undefined, 4, "6", "hello", null])) // [undefined, null]
