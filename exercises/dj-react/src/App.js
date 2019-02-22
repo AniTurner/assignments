bd2e1571bcd1ff346fb0d498eeebffb251cd600a
Ani Turner <aniturner93@gmail.com>
2019-02-22 11:09:06 -0700

    commit message

--- exercises/dj-react/src/App.js
+++ exercises/dj-react/src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends React.Component {
+    constructor() {
+        super() 
+        this.state = {
+            color1: 'blue'
+        }
+    }
+
+    handleChangeColor = () => {
+        const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'purple']
+        const currentColor = colors[Math.floor(Math.random() * colors.length)]
+        this.setState({
+            color1: currentColor
+        })
+    }
+
+    render() {
+
+        const boxStyles = {
+            width: 200, 
+            height: 200, 
+            border: '1px solid black',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: this.state.color1
+        }
+
+        return (
+            <div>
+                <div style={boxStyles}>Box</div>
+                <button onClick={this.handleChangeColor}>Change Color</button>
+            </div>
+        )
+    }
 }
-
-export default App;
