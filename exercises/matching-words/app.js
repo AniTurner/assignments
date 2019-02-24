76d6bc04936cf4c5dac6beeedcc4ae8b2dad6e18
Ani Turner <aniturner93@gmail.com>
2019-02-23 21:05:49 -0700

    commit message

--- exercises/matching-words/app.js
+++ exercises/matching-words/app.js
@@ -1,19 +1,32 @@
 //Create a function that accepts the pirate's inscription.
 //Create an array to store all repeated words.
 //Only list the repeated words once in the new array.
-//Return the new array.
-const str = ["Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas."]
+// //Return the new array.
+// const str = ["Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas."]
 
-function repeatedWords(str) {
 
-    const arr = str.toLowerCase().split(" ").sort().join("").match(/(.)\1+/g);
-    result = []
-    if (arr != null) {
-        arr.forEach((elem) => {
-          result.push(elem[0]);
-        });
+const str = "Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas.";
+
+function findRepeat(str) {
+  const arr = str.split(' ');
+  const hash = new Map();
+  const result = [];
+  // If repeat the value is false, if no repeat the value is true
+  for (let i = 0; i < arr.length; i++) {
+    if (hash.get(arr[i]) === undefined) {
+      hash.set(arr[i], true);
+    } else {
+      const value = hash.get(arr[i]);
+      if (value) {
+        hash.set(arr[i], !value);
       }
-    
-    return result
+    }
+  }
+  hash.forEach((v, k) => {
+    if (!v)
+      result.push(k);
+  });
+  return result;
 }
-console.log(repeatedWords(result))
+console.log(...findRepeat(str));
+
