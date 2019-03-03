0f63a48b06a0b5d8b18f7cb4e5076fc233073bc0
Ani Turner <aniturner93@gmail.com>
2019-03-03 16:28:31 -0700

    commit message

--- exercises/missing-number-warm-up/app.js
+++ exercises/missing-number-warm-up/app.js
@@ -4,13 +4,24 @@
     //Have the function return all numbers missing within the range.
 
 
-function findMissingNo([3,5,4,8,1,2,7]){
+function findMissingNo(arr){
     //Find and return the missing number(s)
-        
+    let sorrtedArr = arr.sort((a,b) => {
+        return a-b 
+    })
+   for (let i = 0; i < arr.length; i++) {
+       if(sorrtedArr[i+1] - sorrtedArr[i] !=1) {
+           return sorrtedArr[i] + 1
+       }
+   } 
 }
 
-console.log(findMissingNo)
+console.log(findMissingNo([3,5,4,8,1,2,7]))  //Output: 6
+console.log(findMissingNo([2,1,3,5]))        //Output: 4
+console.log(findMissingNo([12,10, 9]))       //Output: 11
+console.log(findMissingNo([-3,0,-2,3,2,-1])) //Output: 1
+
 
         
-        //Output: 6
+       
     
