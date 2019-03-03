da1094820de7d16f4b9bbe4cb29453acbd5fe00e
Ani Turner <aniturner93@gmail.com>
2019-03-03 16:15:20 -0700

    commit message

---
+++ exercises/missing-number-warm-up/app.js
@@ -0,0 +1,16 @@
+//Write a function that returns the missing number from an unsorted array of numbers.
+
+    //Create a function that identifies the range of numbers within a given array.
+    //Have the function return all numbers missing within the range.
+
+
+function findMissingNo([3,5,4,8,1,2,7]){
+    //Find and return the missing number(s)
+        
+}
+
+console.log(findMissingNo)
+
+        
+        //Output: 6
+    
